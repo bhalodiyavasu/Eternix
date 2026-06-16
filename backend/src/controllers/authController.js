a9d47b2a84556a9bd1c1c431849ee74b001c8f4e
bhalodiyavasu <vasubhalodiya49@gmail.com>
2026-06-16 12:10:43 +0530

    feat: implement user registration with validation and error handling

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,38 @@
-const registerUser = (req, res) => {
-    res.status(200).json({
-      message: 'User registered successfully'
-    });
-}
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-module.exports = {registerUser};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({message: "Email already exist"});
+    }
+
+    const hashedPassword = await bcrypt.hash(String(password), 10);
+
+    const user = await User.create({
+      name,
+      email, 
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser };
